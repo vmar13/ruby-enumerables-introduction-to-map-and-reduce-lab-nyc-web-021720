2d0070237f35c6030087c986414bfe611382607d
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 21:00:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,9 +63,6 @@ def reduce_to_any_true(source_array)
     return true if source_array[i] == true 
     i += 1 
   end
-  return true  
+  return false   
 else
-  return false if source_array[i] == false 
-  i += 1 
-end 
-  return false 
+  
