94a75e5c1852d781841f3bcce798722018d694c9
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 20:09:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(source_array)
   double_array = []
   i = 0
   while i < source_array.size do 
-  square_array.push(source_array[i]**1)
+  square_array.push(source_array[i]**source_array)
   i += 1 
 end 
 square_array 
