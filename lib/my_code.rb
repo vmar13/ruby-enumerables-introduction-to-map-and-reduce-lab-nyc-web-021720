a65dbb8921ff81d19055af00ae967974ee864be3
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 18:56:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,9 @@
 def map_to_negativize(source_array)
-  array = []
+  new_array = []
   
+  while i < map_to_negativize.length do
+    map_to_negativize([1, 2, 3, -9])
+  end 
+  i += 1 
 end 
+return new_array 
