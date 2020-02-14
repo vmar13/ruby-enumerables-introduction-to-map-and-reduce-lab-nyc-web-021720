2c431e902efd22c7eeac846c2dfc7372db792451
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 19:54:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,6 +9,12 @@ new_array
 end 
 
 def map_to_no_change(source_array)
-  original_array[0] = [source_array]
+  original_array = []
+  i = 0
+  while i < source_array.size do 
+  original_array.push(source_array)
+  i += 1 
 end 
+original_array 
+end  
   
