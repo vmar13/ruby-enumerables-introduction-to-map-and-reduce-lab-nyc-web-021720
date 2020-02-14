3c33800e22560231e79dffb3de450440e7a64a94
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 20:49:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.size do
-    return false if source_array[i] == true 
+    return false if source_array[i] == false 
     i += 1 
   end
   return true  
